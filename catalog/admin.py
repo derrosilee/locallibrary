0a9b82552cef19fe4f2e8bb9b55efa7d6e0e4a04
derrosilee <derrossilee@gmail.com>
2023-04-03 12:26:06 +0300

    Updated Author admin view

--- catalog/admin.py
+++ catalog/admin.py
@@ -25,6 +25,14 @@ class BookAdmin(admin.ModelAdmin):
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
 
 
 # admin.site.register(Book)
