399e7905793f53245f21d7fa1e57f9dda2831283
derrosilee <derrossilee@gmail.com>
2023-04-03 11:56:36 +0300

    Updated Author admin view

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance, Language
+
+
 # Register your models here.
 
 
+# Define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+
+# Register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
 admin.site.register(Book)
-admin.site.register(Author)
+# admin.site.register(Author)
 admin.site.register(Genre)
 admin.site.register(BookInstance)
 admin.site.register(Language)
