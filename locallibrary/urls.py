218cdfa91f0961ed83c96853803bc7e5b9849ceb
derrosilee <derrossilee@gmail.com>
2023-04-03 16:02:49 +0300

    Fixed Url issue"

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -6,7 +6,7 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("catalog.urls"))
+    path('catalog/', include("catalog.urls"))
 ]
 
 
