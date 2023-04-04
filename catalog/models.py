f4f00d68027e2cc2b8414bd15e1782fd4cbd038d
derrosilee <derrossilee@gmail.com>
2023-04-04 12:42:38 +0300

    Added due back functionaity

--- catalog/models.py
+++ catalog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse  # Used to generate URLs by reversing the URL patterns
 import uuid  # Required for unique book instances
 from django.contrib.auth.models import User
+from datetime import date
 
 
 class Genre(models.Model):
@@ -86,6 +87,11 @@ class BookInstance(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
+
+    @property
+    def is_overdue(self):
+        """Determines if the book is overdue based on due date and current date."""
+        return bool(self.due_back and date.today() > self.due_back)
 
 
 class Author(models.Model):
