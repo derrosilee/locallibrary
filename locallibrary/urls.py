7d766e401fb3f55909dc0e1856f6ae248d0148a8
derrosilee <derrossilee@gmail.com>
2023-04-04 11:47:32 +0300

    added urls path

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -6,7 +6,8 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include("catalog.urls"))
+    path('catalog/', include("catalog.urls")),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
 
 
