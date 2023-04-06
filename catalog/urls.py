e9ef4372f1134a918d6591b4c17ffe4380408253
derrosilee <derrossilee@gmail.com>
2023-04-06 17:31:24 +0300

    added book create url

--- catalog/urls.py
+++ catalog/urls.py
@@ -20,3 +20,9 @@ urlpatterns += [
     path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
     path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
 ]
+
+# Add URLConf to create, update, and delete books
+urlpatterns += [
+    path('book/create/', views.BookCreate.as_view(), name='book-create'),
+
+]
