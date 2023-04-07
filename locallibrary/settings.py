29cbb763a65c90e14b2468d6c39384d3bcb95681
derrosilee <derrossilee@gmail.com>
2023-04-07 09:11:50 +0300

    added static root

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -107,9 +107,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+# The absolute path to the directory where collectstatic will collect static files for deployment.
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATIC_URL = 'static/'
-
+# The URL to use when referring to static files (where they will be served from)
+STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
