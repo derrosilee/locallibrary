03548cd7f0c22e5322803767d6a69e9ebf9f43e1
derrosilee <derrossilee@gmail.com>
2023-04-07 06:18:55 +0300

    fixed bug issue with capitalizing 1st letter in the label

--- catalog/models.py
+++ catalog/models.py
@@ -95,15 +95,12 @@ class BookInstance(models.Model):
         return bool(self.due_back and date.today() > self.due_back)
 
 
-
-
-
 class Author(models.Model):
     """Model representing an author."""
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
-    date_of_death = models.DateField('Died', null=True, blank=True)
+    date_of_death = models.DateField('died', null=True, blank=True)
 
     class Meta:
         ordering = ['last_name', 'first_name']
